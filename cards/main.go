bb8f14fc609ccdd25381f0529ae399402a21e884
adan-shahid <adanshahid.student@gmail.com>
2025-07-02 09:28:11 +0500

    Update main.go

--- cards/main.go
+++ cards/main.go
@@ -3,7 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	// var card string = "Ace of Shades"
-	card := "Ace of Shades."
+	// var card string 
+	card := newCard()
 	fmt.Println(card)
+}
+
+func newCard() string {
+	return "Five of Diamonds"
 }
