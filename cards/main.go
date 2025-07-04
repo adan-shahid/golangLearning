428c5d8da8ba4c272c014a23c69300e406ee6e62
adan-shahid <adanshahid.student@gmail.com>
2025-07-04 10:14:58 +0500

    Updated main.go

--- cards/main.go
+++ cards/main.go
@@ -1,13 +1,20 @@
 package main
 
-import "fmt"
+import (
+		"fmt"
+	   "time")
 
 func main() {
 	// var card string 
-	card := newCard()
-	fmt.Println(card)
+	//card := newCard()
+	// WE ARE DEFINING SLICE TO TYPE STRING
+	cards := []string{"Ace of Diamonds", newCard()}
+	fmt.Println(cards)
+	now := time.Now()
+	fmt.Println(now)
+
 }
 
 func newCard() string {
 	return "Five of Diamonds"
-}
+}	
