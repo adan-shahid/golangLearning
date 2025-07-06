5a8340b2760d8d5f040df1296bffdf2abd8ea4ab
adan-shahid <adanshahid.student@gmail.com>
2025-07-06 13:01:23 +0500

    Updated golanglearning/main.go

--- main.go
+++ main.go
@@ -2,6 +2,16 @@ package main
 
 import "fmt"
 
-func main() {
-	fmt.Println("Hi There!")
+// func main() {
+// 	fmt.Println("Hi There!")
+// }
+
+// HEXADECIMAL REPRESENTATION OF A NUMBER
+
+func main(){
+	number := 2586
+	fmt.Printf("%x\n", number)
+	fmt.Printf("Decimal : %d", number)
 }
+
+// TO PRINT A NUMBER IN BASE 10
