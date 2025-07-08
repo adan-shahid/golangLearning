d316f5cde2b316b2163dda02388f3453a0ebd5a2
adan-shahid <adanshahid.student@gmail.com>
2025-07-08 11:31:20 +0500

    Functions implementation in GO

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
 
 // func main() {
 // 	fmt.Println("Hi There!")
@@ -9,9 +12,25 @@ import "fmt"
 // HEXADECIMAL REPRESENTATION OF A NUMBER
 
 func main(){
-	number := 2586
-	fmt.Printf("%x\n", number)
-	fmt.Printf("Decimal : %d", number)
+	// number := 2586
+	// fmt.Printf("%x\n", number)
+	// fmt.Printf("Decimal : %d", number)
+	hotel_name := "The Gopher Hotel"
+	fmt.Println("Hotel Name: "+hotel_name)
+	var roomsAvailable int
+	var rooms, roomsOccupied = 100, rand.Intn(100)
+	roomsAvailable = rooms - roomsOccupied
+	fmt.Println(roomsAvailable, "Rooms Available")
+	age_of_person()
+
+}
+
+func age_of_person(){
+	var agePaul, ageJohn int = rand.Intn(110), rand.Intn(110)
+	fmt.Println("John is", ageJohn , "years old")
+	fmt.Println("Paul is", agePaul , "years old")
+	fmt.Println("It is", ageJohn > agePaul, "that John is older than Paul")
+	fmt.Println("It is", ageJohn == agePaul, "that John and Paul have the same age.")
 }
 
 // TO PRINT A NUMBER IN BASE 10
