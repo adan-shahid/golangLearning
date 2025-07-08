c6507b069244e166991df80ca9ada00256c19333
adan-shahid <adanshahid.student@gmail.com>
2025-07-08 12:01:51 +0500

    Writing the assignment about if/else conditions

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
+
 import (
 	"fmt"
 	"math/rand"
+	
 )
 
 // func main() {
@@ -22,6 +24,8 @@ func main(){
 	roomsAvailable = rooms - roomsOccupied
 	fmt.Println(roomsAvailable, "Rooms Available")
 	age_of_person()
+	email_send()
+	assignment()
 
 }
 
@@ -31,10 +35,46 @@ func age_of_person(){
 	fmt.Println("Paul is", agePaul , "years old")
 
 	if agePaul > ageJohn {
-		fmt.Println("Paul is older than John")
-	} else {
-		fmt.Println("Paul is younger than John, or both have the same age")
-	}
+		fmt.Println("Paul is older than John")}
+	fmt.Println("End of the Program")	
+	// 	else {
+	// 	// ANOTHER IF/ELSE STRUCTURE
+	// 	if agePaul == ageJohn {
+	// 		fmt.Println("Paul and John have the same age.")
+	// 	} else {
+	// 		fmt.Println("Paul is younger than John")
+	// 	}
+	// }
 }
 
-// TO PRINT A NUMBER IN BASE 10
+func email_send(){
+	const emailToSend = 4
+	emailSent := 0
+	for emailSent < emailToSend {
+		fmt.Println("Sending email...")
+		emailSent++
+	}
+	fmt.Println("end of program")
+}
+
+func assignment(){
+	const no_of_rooms = 134
+	var roomsAvailable, roomsBooked int
+	roomsBooked = rand.Intn(110) 
+	roomsAvailable = no_of_rooms - roomsBooked
+
+	if roomsAvailable == 0{
+		fmt.Println("No rooms available for tonight")
+	} else if roomsAvailable == 3 {
+		fmt.Println("- 110 : 2 people / 3 nights \n, - 111 : 4 people / 1 nights \n, - 112 : 6 people / 12 nights \n")
+	}
+
+	var occupancy_rate = (roomsBooked / no_of_rooms) * 100
+	if occupancy_rate <= 30 {
+		fmt.Println("Low")
+	} else if occupancy_rate > 30 && occupancy_rate < 60 {
+		fmt.Println("Medium")
+	} else {
+		fmt.Println("High")
+	}
+}
