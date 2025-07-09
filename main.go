f0816ceba851b825375e52b7ef88f56b0789d615
adan-shahid <adanshahid.student@gmail.com>
2025-07-09 17:17:20 +0500

    Practiced the functions with this assignment

--- main.go
+++ main.go
@@ -1,10 +1,9 @@
 package main
 
-
 import (
 	"fmt"
+
 	"math/rand"
-	
 )
 
 // func main() {
@@ -14,11 +13,39 @@ import (
 // HEXADECIMAL REPRESENTATION OF A NUMBER
 
 func main(){
+	hotel_name := "The Gopher Hotel"
+	
+	const no_of_rooms = 134
+	const firstRoomNumber = 110
+	var roomsAvailable, roomsBooked int
+	roomsBooked = rand.Intn(110) 
+	roomsAvailable = no_of_rooms - roomsBooked
+
+	occupancyRate := occupancyRate(roomsBooked, no_of_rooms)
+	occupancyLevel := occupancyLevel(occupancyRate)
+
+	fmt.Println("Hotel Name: ", hotel_name)
+	fmt.Println("Total Rooms ",no_of_rooms)
+	fmt.Println("Rooms Available", roomsAvailable)
+	fmt.Println("                   Occupancy Level", occupancyLevel)
+	fmt.Println("                   Occupancy Rate", occupancyRate)
+
+	if roomsAvailable > 0 {
+		fmt.Println("Rooms:")
+		for i := 0; roomsAvailable > i; i++ {
+			roomNumber := firstRoomNumber + i
+			size := rand.Intn(6) + 1
+			nights := rand.Intn(10) + 1
+			printRoomDetails(roomNumber, size, nights)
+		}
+	} else {
+		fmt.Println("No rooms available for tonight")
+	}
+
 	// number := 2586
 	// fmt.Printf("%x\n", number)
 	// fmt.Printf("Decimal : %d", number)
-	hotel_name := "The Gopher Hotel"
-	fmt.Println("Hotel Name: "+hotel_name)
+	
 	// var roomsAvailable int
 	// var rooms, roomsOccupied = 100, rand.Intn(100)
 	// roomsAvailable = rooms - roomsOccupied
@@ -28,8 +55,25 @@ func main(){
 	// assignment()
 	// johnPrice := computePrice(148.0, 2)
 	// fmt.Println("John:", johnPrice)
+}
 
+func printRoomDetails(roomNumber, size, nights int){
+	fmt.Println(roomNumber, ":", size, "People /", nights, "nights")
+}
 
+func occupancyLevel (occupancyRate float32) string {
+	if occupancyRate > 70 {
+		return "High"
+	} else if occupancyRate > 20 {
+		return "Medium"
+	} else {
+		return "Low"
+	}
+
+}
+
+func occupancyRate (roomsOccupied int, no_of_rooms int) float32{
+	return (float32(roomsOccupied) / float32(no_of_rooms)) * 100
 }
 
 // func age_of_person(){
@@ -48,7 +92,7 @@ func main(){
 	// 		fmt.Println("Paul is younger than John")
 	// 	}
 	// }
-}
+// }
 
 // func email_send(){
 // 	const emailToSend = 4
@@ -60,27 +104,7 @@ func main(){
 // 	fmt.Println("end of program")
 // }
 
-func assignment(){
-	const no_of_rooms = 134
-	var roomsAvailable, roomsBooked int
-	roomsBooked = rand.Intn(110) 
-	roomsAvailable = no_of_rooms - roomsBooked
 
-	if roomsAvailable == 0{
-		fmt.Println("No rooms available for tonight")
-	} else if roomsAvailable == 3 {
-		fmt.Println("- 110 : 2 people / 3 nights \n, - 111 : 4 people / 1 nights \n, - 112 : 6 people / 12 nights \n")
-	}
-
-	var occupancy_rate = (roomsBooked / no_of_rooms) * 100
-	if occupancy_rate <= 30 {
-		fmt.Println("Low")
-	} else if occupancy_rate > 30 && occupancy_rate < 60 {
-		fmt.Println("Medium")
-	} else {
-		fmt.Println("High")
-	}
-}
 
 // func computePrice(rate float32, nights int) float32 {
 // 	return rate * float32(nights)
